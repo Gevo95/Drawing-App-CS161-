ef92fbe879e3f92b1ed366a1c66aa5681dc31fd6
Gevo95 <146680244+Gevo95@users.noreply.github.com>
2023-11-28 20:58:46 -0800

    Update drawV1.py

--- drawV1.py
+++ drawV1.py
@@ -6,15 +6,27 @@ pygame.init()
 # sets the dimensions for the painting window
 WIDTH, HEIGHT = 500,300
 BG_COLOR = (0, 0, 0) # 0 0 0 is a black background white would be 255 255 255 (look up RGB python colors) 
-DRAW_COLOR = (5, 5, 5)
+DRAW_COLOR = (255, 255, 255)
 LINE_WIDTH = 3 
 
-
+# this sections creates the pygame window 
 screen = pygame.display.set.mode((WIDTH, HEIGHT))
 pygame.display.set_caption("VERSION ONE PAINTER ")
 
-drawing = False 
-last_pos = None
+drawing = False # mouse held down will equal painting/ True   
+last_pos = None # stores the last mouse position. 
+
+
 ### We need a while loop for the main loop of the game to handle the various actions/ events. 
 while True: 
+    screen. fill (BG_COLOR) # TEST to see if code will generate screen
+
+for event in pygame.event.get(): ## this is so user can quit the game 
+    if event.type == pygame.QUIT:
+        pygame.quit()
+        sys.exit
+
+pygame.display.flip # updates display 
+    
+    
     
