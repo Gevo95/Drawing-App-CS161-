c1bca234970b4b8010bf8e1eb876c6fa8555751e
Gevo95 <146680244+Gevo95@users.noreply.github.com>
2023-11-29 22:54:02 -0800

    Update drawV1.py

--- drawV1.py
+++ drawV1.py
@@ -5,10 +5,11 @@ import sys
 pygame.init()
 
 # sets the dimensions for the painting window this can be changed just made it small 
-WIDTH, HEIGHT = 500,300
+WIDTH, HEIGHT = 750, 300
 BG_COLOR = (0, 0, 0) # 0 0 0 is a black background white would be 255 255 255 (look up RGB python colors) 
 DRAW_COLOR = (255, 255, 0) # yellow not sure how to do multi color options
-LINE_WIDTH = 3 
+LINE_WIDTH = 3   
+ERASER_WIDTH = 4 
 
 # this sections creates the pygame window 
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -16,13 +17,15 @@ pygame.display.set_caption("VERSION ONE PAINTER ")
 
 drawing = False # mouse held down will equal painting/ True   
 last_pos = None # stores the last mouse position. 
-erase = True
+erase = False
+
 clock = pygame.time.Clock() # there seem to be some input lag wonder if its the same for you adjusting FPS is a way to limit how much CPU game uses 
 FPS = 30
 
 ### We need a while loop for the main loop of the game to handle the various actions/ events. 
 while True:
    clock.tick(FPS) # part of FPS capping . 
+   
    for event in pygame.event.get(): 
     if event.type == pygame.QUIT: ## this is so user can quit the game 
         pygame.quit()
@@ -42,7 +45,7 @@ while True:
           if not erase:
             pygame.draw.line(screen, DRAW_COLOR , last_pos, current_pos,LINE_WIDTH)
           else:
-             pygame.draw.line(screen,BG_COLOR,last_pos,current_pos,LINE_WIDTH) 
+             pygame.draw.line(screen,BG_COLOR,last_pos,current_pos,ERASER_WIDTH) 
           last_pos = current_pos
  
 
